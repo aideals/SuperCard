d4c2f2d5fa059c309550fdb7dc135e33f1145601
刘鹏 <lp_aideals@hotmail.com>
2017-01-12 17:10:46 +0800

    Initial Commit

---
+++ SuperCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperCard
+//
+//  Created by 鹏 刘 on 2017/1/12.
+//  Copyright © 2017年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
